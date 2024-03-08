3568f9aa6f685c056c36ad10e7b9e082c99ab34d
Kamal <swarnkar889@gmail.com>
2024-03-08 23:00:08 +0530

    Add files via upload

---
+++ HCF & LCM.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int n, n1, n2, l, h;
+
+    printf("num1: ");
+    scanf("%d", &n1);
+
+    printf("num2: ");
+    scanf("%d", &n2);
+
+    if(n1 > n2){
+        n = n2;
+    } else{
+        n = n1;
+    }   // hcf of 2 num(s) will  be lesser than the smaller no
+
+    for(int i = 1; i <= n; i++){
+        if((n1 % i == 0) && (n2 % i == 0)){
+            h = i;
+        }
+    }
+
+    l = (n1 * n2) / h;
+
+    printf("HCF = %d\nLCM = %d", h, l);
+    return 0;
+}
